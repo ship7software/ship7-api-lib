1cf9dbe9271dd6ff93eae4fcee894e4a2ab61da9
hermogenes-ciandt <hermogenes@ciandt.com>
2017-06-22 11:04:39 -0300

    Errors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = {
-	Controller: require('./lib/controller'),
-	Facade: require('./lib/facade'),
-	Crypto: require('./lib/crypto'),
-	Error: require('./lib/error'),
-	Router: require('./lib/router'),
-	Mail: require('./lib/mail'),
+	Controller: require('./lib/controller.js'),
+	Facade: require('./lib/facade.js'),
+	Crypto: require('./lib/crypto.js'),
+	Error: require('./lib/error.js'),
+	Router: require('./lib/router.js'),
+	Mail: require('./lib/mail.js'),
 	Test: {
-		Rest: require('./lib/test/rest')
+		Rest: require('./lib/test/rest.js')
 	}
 };
