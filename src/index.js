2fda55706aae74d202985ccb6318e7847cd1959b
hermogenes-ciandt <hermogenes@ciandt.com>
2017-06-26 13:01:05 -0300

    Pagination

--- src/index.js
+++ src/index.js
@@ -14,10 +14,31 @@ class Controller {
     this.facade = facade;
   }
 
+
   find(req, res, next) {
-    return this.facade.find(req.query)
-    .then(collection => res.status(200).json(collection))
-    .catch(err => next(err));
+    if(this.facade.Schema.collection.collectionName != 'empresas')
+      req.query.empresaId = new ObjectId(req.usuario.empresaId._id)
+    let query = req.query
+    const sort = query.sort
+    const limit = query.limit
+    const skip = query.skip
+    delete query['sort']
+    delete query['limit']
+    delete query['skip'] 
+
+    if(skip && limit) {
+      this.facade.count(query).then(count => {
+        return this.facade.findWithPagination(req.query, null, sort, limit, skip)
+        .then(collection => {
+          res.status(200).json({ items: collection, total: count }) 
+        }).catch(err => { next(err) })
+      }).catch(err => { next(err) })
+    }
+    else {
+      return this.facade.find(req.query)
+      .then(collection => res.status(200).json(collection))
+      .catch(err => { next(err) })
+    }
   }
 
   findOne(req, res, next) {
@@ -121,6 +142,15 @@ class Facade {
     return this.Schema
     .findById(id)
     .exec();
+  }
+
+  findWithPagination(query, projection, sort, limit, skip) {
+    return this.Schema
+    .find(query, projection)
+    .sort(sort)
+    .skip(Number(skip))
+    .limit(Number(limit))
+    .exec()  
   }
 
   remove(id) {
